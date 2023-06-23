d459619c2f011cc61906fa4e5f5620ad793fca86
omar554 <62614708+omar554@users.noreply.github.com>
2023-06-23 16:02:48 -0600

    Add: Register

--- app/taller4/src/App.js
+++ app/taller4/src/App.js
@@ -4,11 +4,12 @@ import SongCard from './components/SongCard';
 import AllPlaylist from './pages/AllPlaylist';
 import Allsongs from './pages/Allsongs';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <div className="App">
-    <Login />
+    <Register/>
     </div>
   );
 }
---
+++ app/taller4/src/pages/Register.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+
+const Register = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Realizar lógica de registro o enviar datos a un servidor
+
+    // Reiniciar los campos del formulario
+    setName('');
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+        <h2 className="text-2xl text-center font-bold mb-6">Registro</h2>
+
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+            Nombre
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="name"
+            type="text"
+            placeholder="Ingrese su nombre"
+            value={name}
+            onChange={handleNameChange}
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Ingrese su email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            Contraseña
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="Ingrese su contraseña"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+
+        <div className="flex items-center justify-center">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Registrarse
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
