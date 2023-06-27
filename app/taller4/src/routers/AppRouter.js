733afb00f585102c31aaf07da846b1e3bde6ca01
EA03 <00175420@uca.edu.sv>
2023-06-27 13:52:51 -0600

    :construction:bring songs

--- app/taller4/src/routers/AppRouter.js
+++ app/taller4/src/routers/AppRouter.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from "../pages/Login";
 import AllPlaylist from "../pages/AllPlaylist";
+import Allsongs from "../pages/Allsongs";
+
 
 
 export const AppRouter = () => {
@@ -11,6 +13,7 @@ export const AppRouter = () => {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/allplaylist" element={<AllPlaylist />} />
+          <Route path="/allsongs" element={<Allsongs/>} />
         </Routes>
       </div>
     </Router>
