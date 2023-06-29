0e609f6ad4afaebf0bfe796294f594c1cc363801
EA03 <00175420@uca.edu.sv>
2023-06-29 17:12:27 -0600

    :lipstick:Update Allplaylist and Allsongs views

--- app/taller4/src/pages/Allsongs.jsx
+++ app/taller4/src/pages/Allsongs.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import SongCard from '../components/SongCard';
 import AppServices from '../services/AppServices';
 import context from '../context/UserContext';
+import Buttons from '../components/Buttons';
 
 const AllSongs = () => {
   const [songs, setSongs] = useState([]);
@@ -13,9 +14,9 @@ const AllSongs = () => {
   const fetchAllSongs = async () => {
     let token = context.getToken();
     console.log(token);
-    let response = await AppServices.fetchAllSongs(token, 0, 5);
+    let response = await AppServices.fetchAllSongs(token, 0, 10);
     console.log(response);
-    
+
 
     if (!response.error) {
       let data = response;
@@ -24,18 +25,23 @@ const AllSongs = () => {
   };
 
   return (
-    <div className="flex flex-col items-center gap-4">
-      <h1 className="text-2xl font-bold mb-4 mt-4">Todas las Canciones</h1>
-      {songs && songs.map((post) => (
-        <SongCard
-          key={post._id}
-          isMainView={true}
-          code={post._id}
-          title={post.title}
-          duration={post.duration}
-        />
-      ))}
-    </div>
+    <>
+      <div className="mb-6">
+        <h1 className="text-4xl font-bold m-4">Todas las Canciones</h1>
+        <div className='grid grid-cols-3 px-32 gap-10 mt-8'>
+          {songs && songs.map((post) => (
+            <SongCard
+              key={post._id}
+              isMainView={true}
+              code={post._id}
+              title={post.title}
+              duration={post.duration}
+            />
+          ))}
+        </div>
+      </div>
+      <Buttons />
+    </>
   );
 };
 
